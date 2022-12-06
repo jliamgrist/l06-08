d4e3c1f6baeae02069bf7e845f4f9c9d33af93b5
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2022-12-05 19:01:28 -0700

    css stuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let storeHours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm","3pm", "4pm", "5pm","6pm", "7pm"];
 
 let objSeattle = {
-  name: "seattle",
+  name: "Seattle",
   min: 23,
   max: 65,
   avgCookiesPerSale: 6.3,
