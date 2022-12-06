9c1c1a5dbe86f7c9e89d1967aa01fa27f91f1b1d
cloudnalu <68409311+cloudnalu@users.noreply.github.com>
2022-12-05 18:48:12 -0700

    rendering to DOM seattle and tokyo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let storeHours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm","3pm", "4pm", "5pm","6pm", "7pm"];
 
 let objSeattle = {
+  name: "seattle",
   min: 23,
   max: 65,
   avgCookiesPerSale: 6.3,
   getNumOfCookiesPerHour: function () {
     for (let index = 0; index < storeHours.length; index++) {
-      let calculateCookies = Math.floor(salesPerHour() * this.avgCookiesPerSale)
+      let calculateCookies = Math.floor(seattleSalesPerHour() * this.avgCookiesPerSale)
       this.numOfCookiesPerHour.push(calculateCookies)
       // console.log(calculateCookies)
     }
@@ -21,9 +22,37 @@ objSeattle.getNumOfCookiesPerHour();
 
 // random number of customers per hour
 
-function salesPerHour(min, max) {
+function seattleSalesPerHour(min, max) {
   min = objSeattle.min;
   max = objSeattle.max;
+  return Math.floor(Math.random() * (max - min) + min);
+} 
+
+let objTokyo = {
+  name: "Tokyo",
+  min: 3,
+  max: 24,
+  avgCookiesPerSale: 1.2,
+  getNumOfCookiesPerHour: function () {
+    for (let index = 0; index < storeHours.length; index++) {
+      let calculateCookies = Math.floor(tokyoSalesPerHour() * this.avgCookiesPerSale)
+      this.numOfCookiesPerHour.push(calculateCookies)
+      // console.log(calculateCookies)
+    }
+  },
+  numOfCookiesPerHour: []
+
+}
+// console.log(objSeattle.numOfCookiesPerHour);
+
+objTokyo.getNumOfCookiesPerHour();
+// console.log(objSeattle);
+
+// random number of customers per hour
+
+function tokyoSalesPerHour(min, max) {
+  min = objTokyo.min;
+  max = objTokyo.max;
   return Math.floor(Math.random() * (max - min) + min);
 } 
 
@@ -48,6 +77,59 @@ function render (storeObject) {
   }
   return dataArr;
 }
-render(objSeattle);
+let seattleListItems = render(objSeattle);
+let tokyoListItems = render(objTokyo);
 console.log(render(objSeattle));
+console.log(render(objTokyo));
 // console.log(dataArr);
+
+
+// we need to:
+// get (from the DOM) who the parent element is going to be. where am I attaching this new element
+const parentElement = document.getElementById('storeProfiles');
+
+// create a new element, or elements, that represent each store
+// article
+// inside that article, h2 for the name, paragraph with their age, ul and some lis with their interests, image
+const article = document.createElement('article');
+parentElement.appendChild(article);
+
+const h2 = document.createElement('h2');
+h2.textContent = objSeattle.name;
+article.appendChild(h2);
+
+const p = document.createElement('p');
+p.textContent = 'This store is located in: ' + objSeattle.name;
+article.appendChild(p);
+
+const ul = document.createElement('ul');
+article.appendChild(ul);
+
+for (let i = 0; i < seattleListItems.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = seattleListItems[i];
+  ul.appendChild(li);
+}
+
+// TOKYO
+
+const h2b = document.createElement('h2');
+h2b.textContent = objTokyo.name;
+article.appendChild(h2b);
+
+const pb = document.createElement('p');
+pb.textContent = 'This store is located in: ' + objTokyo.name;
+article.appendChild(pb);
+
+const ulb = document.createElement('ul');
+article.appendChild(ulb);
+
+for (let i = 0; i < tokyoListItems.length; i++) {
+  const lib = document.createElement('li');
+  lib.textContent = tokyoListItems[i];
+  ulb.appendChild(lib);
+}
+
+
+
+
